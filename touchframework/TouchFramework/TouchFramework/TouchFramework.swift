d05fb35196429f90ee107ab62253e6bd0d7bcd37
Arunabh Das <arunabhdas@gmail.com>
2019-10-08 17:05:27 -0400

    Added TouchFramework class

---
+++ touchframework/TouchFramework/TouchFramework/TouchFramework.swift
@@ -0,0 +1,17 @@
+//
+//  TouchFramework.swift
+//  TouchFramework
+//
+//  Created by Das on 10/8/19.
+//  Copyright © 2019 Arunabh Das. All rights reserved.
+//
+
+import UIKit
+
+open class TouchFramework: NSObject {
+    
+    open class func logToConsole(_ msg: String) {
+        print(msg);
+    }
+
+}
