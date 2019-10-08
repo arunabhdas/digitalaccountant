04d6747801dc92893a8db51ac8e74868fb5f00be
Arunabh Das <arunabhdas@gmail.com>
2019-10-08 16:53:34 -0400

    Init TouchFramework

---
+++ touchframework/TouchFramework/TouchFramework/TouchFramework.h
@@ -0,0 +1,19 @@
+//
+//  TouchFramework.h
+//  TouchFramework
+//
+//  Created by Das on 10/8/19.
+//  Copyright © 2019 Arunabh Das. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TouchFramework.
+FOUNDATION_EXPORT double TouchFrameworkVersionNumber;
+
+//! Project version string for TouchFramework.
+FOUNDATION_EXPORT const unsigned char TouchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TouchFramework/PublicHeader.h>
+
+
